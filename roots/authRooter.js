3f1c5adf53c961097792dd415d4d5fdbec9e056d
JerryLunda <jerrylundakasongo@gmail.com>
2024-02-14 18:18:57 +0200

    implement schema of database

--- roots/authRooter.js
+++ roots/authRooter.js
@@ -4,8 +4,5 @@ const authController = require('../src/controllers/authController');
 const passport = require('../src/passport_config')
 
 router.post('/login', authController.login);
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res)=>{
-    res.send('Welcome to your profile')
-});
 
 module.exports = router;
